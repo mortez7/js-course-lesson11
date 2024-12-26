1e96858c5f85eea89b8702b9a6f7715928290247
Ivan Sokolenko <ivan.sokolenko.2017@gmail.com>
2024-12-26 14:57:26 +0200

    Выполнено задание урока №11

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
+"use strict";
 
+const square = document.querySelector("#square");
+const circle = document.querySelector("#circle");
+const btn = document.querySelector("#btn");
+const eBtn = document.querySelector("#e_btn");
+const input = document.querySelector("#text");
+const range = document.querySelector("#range");
+const rangeSpan = document.querySelector("#range-span");
 
+btn.addEventListener("click", function (event) {
+  square.style.backgroundColor = input.value;
+  input.value = "";
+});
 
+eBtn.style.display = "none";
 
-
+range.addEventListener("input", function (event) {
+  rangeSpan.textContent = `${range.value}%`;
+  circle.style.height = `${range.value}%`;
+  circle.style.width = `${range.value}%`;
+});
